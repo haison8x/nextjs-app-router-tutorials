4c2234843b7618c5a044ca0e77fc1348e8538426
jacob.phan <jacob.phan@starixsoft.com>
2025-12-22 16:51:37 +0700

    feat: chapter 15

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 200));
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
     console.log(`revenue data fetching completed at ${new Date().toISOString()}`);
     // console.log('Data fetch completed after 3 seconds.');
@@ -32,7 +32,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     console.log('Fetching latest invoices...');
-    await new Promise((resolve) => setTimeout(resolve, 500));
+    await new Promise((resolve) => setTimeout(resolve, 100));
 
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
